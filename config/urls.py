e6bf726dc12e57612417ab58d52247aa7525423e
Adarsh ID <acw462011@gmail.com>
2026-02-04 19:14:52 +0530

    final pushig

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,10 +10,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('/', include('core.urls')),
-]
->>>>>>> 881d22fefbe9d65904b95e8173912a6016cb4e52
