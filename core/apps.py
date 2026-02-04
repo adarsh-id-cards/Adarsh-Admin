59b3ad75b60562baebc1a4201c53b0a0e0233902
Adarsh ID <acw462011@gmail.com>
2026-02-04 20:12:33 +0530

    Auto migrate and create superuser without shell

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
-
+import os
 
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
+
+    def ready(self):
+        # prevent running during migrations or management commands
+        if os.getenv("RUN_MAIN") != "true":
+            return
+
+        from core.utils.run_migrations import run_migrations
+        from core.utils.create_superuser import create_superuser_if_needed
+
+        run_migrations()
+        create_superuser_if_needed()
--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -2,3 +2,4 @@
 Core Utilities Package
 """
 from .email_utils import generate_secure_password, send_welcome_email
+default_app_config = "core.apps.CoreConfig"
